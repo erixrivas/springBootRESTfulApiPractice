343a454f707b06af6edca894031dc19f8f078ea6
Erix Rivas <erixrivas@gmail.com>
2021-11-20 15:52:39 -0500

    first commit

---
+++ src/main/java/com/erixrivas/springBootRESTfulApiPractice/SpringBootResTfulApiPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.erixrivas.springBootRESTfulApiPractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootResTfulApiPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootResTfulApiPracticeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/erixrivas/springBootRESTfulApiPractice/SpringBootResTfulApiPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.erixrivas.springBootRESTfulApiPractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootResTfulApiPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
